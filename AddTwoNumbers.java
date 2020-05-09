e4226284c8affb61af59955265782eb405c8a48e
mishra28soumya <mishra28soumya@gmail.com>
2020-05-09 13:00:15 +0530

    Create AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,51 @@
+/* You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807. */
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = null;
+        ListNode prev = null;
+        ListNode temp = null;
+        int carry=0, sum;
+        while(l1!=null || l2!=null){
+            sum = carry + (l1!=null?l1.val:0) + (l2!=null?l2.val:0);
+            carry = (sum>9)?1:0;
+            sum = sum%10;
+            temp = new ListNode(sum);
+            if(res == null){
+                res = temp;
+            } else {
+                prev.next = temp;
+            }
+            prev = temp;
+            if(l1!=null){
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
+            }
+        }
+        if(carry == 1){
+            temp = new ListNode(carry);
+            prev.next = temp;
+        }
+        return res;
+    }
+}
